a8ee41a84347d3e2115ecc26f792394d38c49a9a
Tom Lee <jcblee@gmail.com>
2021-10-15 17:04:41 -0400

    header, footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,119 @@
 <template>
   <div id="app">
+    <head>
+      <meta charset="utf-8" />
+      <title>UConn Banner</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <link :href="require('./assets/banner.css')" rel="stylesheet" />
+    </head>
+    <div id="uconn-banner">
+      <div id="uconn-header-container">
+        <div class="row-container">
+          <div class="row-fluid">
+            <div id="home-link-container">
+              <a id="home-link" href="http://uconn.edu/">
+                <span id="wordmark">UCONN</span>
+                <span id="university-of-connecticut"
+                  >UNIVERSITY OF CONNECTICUT</span
+                >
+              </a>
+            </div>
+            <div id="button-container">
+              <span>
+                <a class="btn" href="http://uconn.edu/search.php" title="Search"
+                  ><i class="icon-search"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  class="btn"
+                  href="http://uconn.edu/azindex.php"
+                  title="AZ Index"
+                  ><i class="icon-a-z"></i
+                ></a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row-container" id="site-header">
+        <div class="row-fluid">
+          <div>
+            <p id="super-title">
+              <a href="http://lib.uconn.edu/" style="color: white;"
+                >UCONN LIBRARY</a
+              >
+            </p>
+            <h1 id="site-title"><a href="/" style="color: white;">Greenhouse Studios</a></h1>
+          </div>
+          <div id="search-container">
+            <form class="form-search">
+              <input
+                type="text"
+                class="input-medium"
+                placeholder="Search this site..."
+              />
+              <button type="submit" class="btn">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <Nav></Nav>
     <home-button></home-button>
     <router-view></router-view>
     <!-- <home></home> -->
+    <hr>
+    <!-- <footer id="footer" class="site-footer" role="contentinfo">
+			<div class="container">
+				<ul id="uc-footer-links" class="clearfix">
+											<li>
+							© <a href="http://uconn.edu">University of Connecticut</a>
+						</li>
+						<li>
+							<a href="http://uconn.edu/disclaimers-privacy-copyright/">Disclaimers, Privacy &amp; Copyright</a>
+						</li>
+                        <li>
+							<a href="https://accessibility.uconn.edu/">Accessibility</a>
+						</li>
+				</ul>
+			</div>
+		</footer> -->
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HomeButton from './components/HomeButton.vue'
-import Nav from './components/Nav.vue'
+import Footer from './components/Footer.vue';
+import HomeButton from "./components/HomeButton.vue";
+import Nav from "./components/Nav.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Nav,
-    HomeButton
-  }
-}
+    HomeButton,
+    Footer,
+  },
+};
 </script>
 
 <style>
+footer{
+font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+font-size: 14px;
+line-height: 1.428571429;
+color: #333333;
+display: block;
+}
+#footer {
+padding: 1.5em 0;
+color: rgba(255, 255, 255, 0.7);
+}
+#site-header{
+  background: #9ac840!important;
+}
 </style>
--- src/Home.vue
+++ src/Home.vue
@@ -11,78 +11,7 @@
       <img src="../public/img/Animation_Fill.png" />
     </div>
 
-    <footer>
-      <img class="footer-img" src="../public/img/watercolor-footer.png" />
-      <section class="footer-information">
-        <div class="contact-info">
-          <p>Greenhouse Studios</p>
-          <p>Homer Babbidge Library, Level One</p>
-          <p>369 Fairfield Way, Unit 1005</p>
-          <p>Storrs, CT 06269-1005</p>
-        </div>
 
-        <div class="social-media">
-          <div class="email">
-            <a href="mailto:greenhousestudios@uconn.edu"
-              ><img src="../public/img/email.svg"
-            /></a>
-          </div>
-          <div class="instagram">
-            <a href="https://www.instagram.com/greenhousestudiosuconn/?hl=en"
-              ><img src="../public/img/instagram.svg"
-            /></a>
-          </div>
-          <div class="youtube">
-            <a href="https://www.youtube.com/channel/UCuDO0yPZYSaXBE3G7ArmQjw"
-              ><img src="../public/img/youtube.svg"
-            /></a>
-          </div>
-          <div class="facebook">
-            <a href="https://www.facebook.com/GreenhouseStudiosUConn/"
-              ><img src="../public/img/facebook.svg"
-            /></a>
-          </div>
-          <div class="twitter">
-            <a href="https://twitter.com/greenhouseuconn?lang=en"
-              ><img src="../public/img/twitter.svg"
-            /></a>
-          </div>
-        </div>
-
-        <div class="footer-pages">
-          <div class="footer-people">
-            <a href="">People</a>
-          </div>
-          <div class="footer-projects">
-            <a href="">Projects</a>
-          </div>
-          <div class="footer-joinus">
-            <a href="">Join Us</a>
-          </div>
-        </div>
-
-        <div class="copyright">
-          <p>© University of Connecticut</p>
-          <p>Disclaimers, Privacy & Copyright</p>
-          <p>Accessbility</p>
-        </div>
-
-        <div class="grants">
-          <img
-            class="uconn-library"
-            src="../public/img/UConn_Library_Logo.png"
-          />
-          <img
-            class="uconn-humanities"
-            src="../public/img/UConn_Humanities_Institute.png"
-          />
-          <img
-            class="mellon"
-            src="../public/img/Andrew_W_Mellon_Foundation_Logo.png"
-          />
-        </div>
-      </section>
-    </footer>
   </div>
 </template>
 
