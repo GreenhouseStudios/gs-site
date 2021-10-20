6bf5e10213d4710e772a153f0523335aad264372
Tom Lee <jcblee@gmail.com>
2021-10-20 13:26:40 -0400

    loading component

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,34 +1,34 @@
-<template><div class="overlay">
+<template>
+  <div class="overlay">
     <div class="container">
-        
-        <img src="../../public/img/GS_G_logo.png" alt="">
-        <h1>Loading...</h1>
-    </div></div>
+      <img src="../../public/img/GS_G_logo.png" alt="" />
+      <h1>Loading...</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-.overlay{
-    position: fixed;
-    top:  0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background: rgba($color: #000000, $alpha: 0.5);
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba($color: #000000, $alpha: 0.5);
+  z-index: 5;
 }
-.container{
-    position: fixed;
-    top: 20vh;
-    left: 30vw;
-    background: white;
-    padding: 10%;
+.container {
+  position: fixed;
+  top: 20vh;
+  left: 30vw;
+  background: white;
+  padding: 10%;
 }
-h1{
-    text-align: center;
+h1 {
+  text-align: center;
 }
 </style>
