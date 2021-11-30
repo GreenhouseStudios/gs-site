704f156b0181028c66ad5257d9c97e30bd0ef688
Tom Lee <jcblee@gmail.com>
2021-11-30 13:36:37 -0500

    no pointer events on bg images

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,7 @@
       :style="`top: ${spacing * i + 500}px; left:${randomXinMargin(i)}px; transform:scale(${width > 600 ? 1 : 2})`"
       v-for="i in Array.from(Array(numBgImages).keys())"
       :key="i"
+      id="bg-image"
     /></div>
     <router-view v-on:subnav-change="childUpdate"></router-view>
     <hr />
@@ -170,6 +171,9 @@ footer {
 }
 .dark-gray{
   color: #808080;
+}
+#bg-image{
+  pointer-events: none;
 }
 :root {
   --main-bg-color: #8CC947;
