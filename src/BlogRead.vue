78054b6fc2d057ad93af715f97bcd488738b928a
Tom Lee <jcblee@gmail.com>
2022-10-12 16:49:12 -0400

    blog read scoped style

--- src/BlogRead.vue
+++ src/BlogRead.vue
@@ -87,7 +87,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@100;200;300;400;500;600;700;800;900&display=swap");
 @import "./assets/blog.css";
 body{
