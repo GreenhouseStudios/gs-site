156e3c8d8701b732a7840df40c29b0106966eb69
Tom Lee <jcblee@gmail.com>
2022-10-26 16:51:31 -0400

    fetch all post pages

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,7 +7,7 @@ const actions = {
   async getSiteData({ commit, dispatch }) {
     await dispatch("getPostCount");
     await dispatch("getPeopleCount");
-    await dispatch("getPosts");
+    await dispatch("getPosts", 1);
     await dispatch("getPeople", 1);
     await dispatch("getProjects");
     commit("setLoadingState", false);
@@ -22,21 +22,21 @@ const actions = {
       commit("setPeopleCount", parseInt(res.headers["x-wp-total"]));
     });
   },
-  async getPosts({ commit }) {
-    return axios.get("posts?per_page=100&page=1").then((res) => {
+  async getPosts({ commit, dispatch }, page) {
+    return axios.get("posts?per_page=100&page=" + page).then((res) => {
+      let pageTotal = parseInt(res.headers["x-wp-totalpages"]);
       var posts = [];
       res.data.forEach((post) => {
-        if (post._links["wp:featuredmedia"]) {
-          axios
-            .get("media/" + post.featured_media)
-            .then((imgSrc) => {
-              post["image"] = imgSrc.data.guid.rendered;
-            })
-            .catch((err) => console.log(err));
-        }
+        // axios.get("media/" + post.featured_media).then((imgSrc) => {
+        //   post["image"] = imgSrc.data.guid.rendered;
+
+        // })
         posts.push(post);
+        commit("setPosts", posts);
       });
-      commit("setPosts", posts);
+      if (page < pageTotal) {
+        dispatch("getPosts", page + 1);
+      }
     });
   },
   async getImage(something, id) {
@@ -44,21 +44,7 @@ const actions = {
       return imgSrc.data.guid.rendered;
     });
   },
-  async getBlogImages({ commit }, posts) {
-    var promises = [];
-    posts.forEach((post) => {
-      if (post.featured_media) {
-        promises.push(
-          axios.get("media/" + post.featured_media).then((imgSrc) => {
-            post["image"] = imgSrc.data.guid.rendered;
-          })
-        );
-      }
-    });
-    Promise.all(promises).then(() => {
-      commit("addPosts", posts);
-    });
-  },
+
   async getMorePosts({ state, dispatch }, page) {
     let remainingPostCount = state.postCount - state.posts.length;
     console.log(`remaining posts: ${remainingPostCount}`);
@@ -75,7 +61,7 @@ const actions = {
         });
     }
   },
-  async getPersonImage({commit},person){
+  async getPersonImage({ commit }, person) {
     if (
       person.categories.indexOf(86) < 0 &&
       person._links["wp:featuredmedia"]
@@ -84,14 +70,10 @@ const actions = {
         let img = imgSrc.data.media_details.sizes;
         if (img) {
           if (img.full && img.full.height < 300)
-            person["image"] =
-              img.full.source_url;
-          else if (img.medium)
-            person["image"] =
-              img.medium;
+            person["image"] = img.full.source_url;
+          else if (img.medium) person["image"] = img.medium;
           else if (img.thumbnail.source_url) {
-            person["image"] =
-              img.thumbnail.source_url;
+            person["image"] = img.thumbnail.source_url;
           } else
             person["image"] =
               "https://dev-greenhouse-studios.pantheonsite.io/wp-content/uploads/2017/01/g_icon-placeholder-1.jpg";
@@ -100,16 +82,16 @@ const actions = {
         commit("addPerson", person);
       });
     }
+    else commit("addPerson", person)
   },
   async getPeople({ dispatch }, page) {
-    console.log(page)
     return axios.get("person?per_page=100&page=" + page).then((res) => {
       let pageTotal = parseInt(res.headers["x-wp-totalpages"]);
       res.data.forEach((person) => {
-        dispatch("getPersonImage",person)
+        dispatch("getPersonImage", person);
       });
       if (page < pageTotal) {
-        dispatch("getPeople", (page + 1))
+        dispatch("getPeople", page + 1);
       }
     });
   },
