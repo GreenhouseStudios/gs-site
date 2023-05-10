93140998f8d65a037e84b20955c1bf4e5f146690
Tom Lee <jcblee@gmail.com>
2023-05-10 10:55:58 -0400

    project card fallbacks

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,11 +6,13 @@
           class="img-front h-100 w-100 overflow-hidden"
         >
            <img
+            v-if="project.custom_fields.project_card_front"
             :src="project.custom_fields.project_card_front"
             class="absolute top-0 z-1"
             alt="project title"
             loading="lazy"
           />
+            <div v-else class="center w-100 h-100 flex items-center ph3 tc absolute"  ><h2 id="alt-card-title" class="f1 ph3 tc v-mid self-center z-3">{{project.title.rendered  }}</h2></div>
         <img class="h-100" loading="lazy" :src="`${require('../../public/img/avif/Watercolor_background.avif')}`" style="object-fit: cover"  :style="`z-index: -1; ${((Math.cos(phase) + 1) / 2) * 100}%; filter: hue-rotate(${
             Math.sin(phase) * 10 + 2
           }deg) `" alt="Front side of flippable card for the project">
@@ -24,16 +26,18 @@
         "
          :style="
            'background-image: url(' +
-           project.custom_fields.project_card_back +
+           (project.custom_fields.project_card_back || defaultFaceBackground )+
            '); background-repeat: no-repeat;'
          "
       >
         <img
+        v-if="project.custom_fields.project_title_img"
           class="title-new"
           :src="project.custom_fields.project_title_img"
-          alt="by our love"
+          :alt="project.title.rendered"
           loading="lazy"
         />
+        <h2 v-else>{{project.title.rendered }}</h2>
         <div class="desc" v-if="project.custom_fields.about">
           <p>
             {{ project.custom_fields.about[0] }}
@@ -99,6 +103,7 @@ export default {
       isFlipped: this.startsFlipped ? true : false,
       flipping: false,
       hover: false,
+      defaultFaceBackground: "https://dev-greenhouse-studios.pantheonsite.io/wp-content/uploads/2021/11/Card_Back_bright_green_new.jpg"
     };
   },
   computed: {
@@ -157,5 +162,12 @@ export default {
 #button {
   position: absolute;
   bottom: 35px;
+}
+#alt-card-title{
+  font-family: 'SAMO';
+  letter-spacing: 0.1rem;
+  text-transform: uppercase;
+  background: none;
+  margin: auto;
 }
 </style>
